10b930a8ce18b2a0bd3d449063837f014c4cb971
Kalpesh Parab <abhi9405.at@gmail.com>
2025-07-04 15:25:18 +0530

    feat - Header animation added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,10 +2,34 @@ import './header.scss';
 import logo from '../../assets/logo.png';
 import royalLogo from '../../assets/royalLogo.png';
 import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const [showHeader, setShowHeader] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY && currentScrollY > 100) {
+        // scrolling down
+        setShowHeader(false);
+      } else {
+        // scrolling up
+        setShowHeader(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [lastScrollY]);
+
   return (
-    <section className='Header'>
+    <section className={`Header ${!showHeader ? 'hide' : ''}`}>
       <div className='logo'>
         <img src={logo} alt='' />
       </div>
