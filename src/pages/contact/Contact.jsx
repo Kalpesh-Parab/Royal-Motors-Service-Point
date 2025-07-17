b65127afb7e52486c453526f467d25fe0379d0cd
Kalpesh Parab <abhi9405.at@gmail.com>
2025-07-17 13:02:35 +0530

    feat - contact us page completed

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -4,8 +4,35 @@ import instw from '../../assets/instw.png';
 import whatsapp from '../../assets/whatsapp.png';
 import work from '../../assets/work.png';
 import mech2 from '../../assets/mech2.png';
+import bigLogo from '../../assets/bigLogo.png';
+import arrow from '../../assets/contact.png';
+import { useRef, useState } from 'react';
+import { toast } from 'react-toastify';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_lm6wua7',
+        'template_bxpz39f',
+        form.current,
+        'cHTX2zDYOc4kN6Vsc'
+      )
+      .then(() => {
+        toast.success('Enquiry submitted successfully');
+        form.current.reset();
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error('Oops! Something went wrong.');
+      });
+  };
+
   const info = [
     {
       icon: map,
@@ -70,7 +97,47 @@ const Contact = () => {
           title='Royal Motors Location'
         ></iframe>
       </div>
-      <div className='form'></div>
+      <div className='bottom'>
+        <form className='form' ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type='text' name='name' placeholder='Your Name' required />
+
+          <label>Choose Your Model</label>
+          <select name='model' required>
+            <option value=''>-- Select --</option>
+            <option>Hunter 360</option>
+            <option>Classic 650</option>
+            <option>Goan Classic 350</option>
+            <option>Scram 440</option>
+            <option>Bear 650</option>
+            <option>Classic 350</option>
+            <option>Guerrilla 450</option>
+            <option>Shotgun 650</option>
+            <option>Himalayan 450</option>
+            <option>Bullet 350</option>
+            <option>Super Meteor 650</option>
+            <option>Meteor 350</option>
+            <option>Interceptor 650</option>
+            <option>Continental GT 650</option>
+          </select>
+
+          <label>Phone No.</label>
+          <input type='tel' name='phone' placeholder='Mobile Number' required />
+
+          <label>Query</label>
+          <input type='text' name='query' placeholder='Your Query' required />
+
+          <button type='submit' className='contactUs'>
+            Submit
+            <div className='arrow'>
+              <img src={arrow} alt='' loading='lazy' />
+            </div>
+          </button>
+        </form>
+        <div className='biglogo'>
+          <img src={bigLogo} alt='' />
+        </div>
+      </div>
     </section>
   );
 };
