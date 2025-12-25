0630f0b127156e81cc494aa0b402070964a3ab0f
Kalpesh Parab <abhi9405.at@gmail.com>
2025-12-26 00:04:29 +0530

    feat admin dashboard

--- src/pages/admin/adminApi.js
+++ src/pages/admin/adminApi.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: 'https://royal-backend-1-hzkn.onrender.com' || 'http://localhost:5000',
 });
 
 API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("admin_token");
+  const token = localStorage.getItem('admin_token');
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
