00f9439f44511b8527f065072fd11737e25834b5
Kalpesh Parab <abhi9405.at@gmail.com>
2025-12-26 00:07:40 +0530

    feat admin dashboard

--- src/pages/admin/Sidebar.jsx
+++ src/pages/admin/Sidebar.jsx
@@ -1,12 +1,12 @@
-import "./sidebar.scss";
+import './sidebar.scss';
 
 export default function Sidebar() {
   return (
-    <aside className="sidebar">
-      <div className="logo">Royal Admin</div>
+    <aside className='sidebar'>
+      <div className='logo'>Royal Admin</div>
 
       <nav>
-        <button className="active">Services</button>
+        <button className='active'>Services</button>
         <button disabled>Leads</button>
         <button disabled>Invoices</button>
       </nav>
