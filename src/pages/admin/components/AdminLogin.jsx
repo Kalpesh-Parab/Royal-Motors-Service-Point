39658031368665601e4cde8144adc0f67760ad6d
Kalpesh Parab <abhi9405.at@gmail.com>
2025-12-26 00:26:11 +0530

    feat admin dashboard

--- src/pages/admin/components/AdminLogin.jsx
+++ src/pages/admin/components/AdminLogin.jsx
@@ -1,38 +1,41 @@
-import { useState } from "react";
-import "./adminLogin.scss";
-import axios from "axios";
-import { toast } from "react-toastify";
+import { useState } from 'react';
+import './adminLogin.scss';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 export default function AdminLogin({ onSuccess }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/auth/login", {
-        email,
-        password,
-      });
+      const res = await axios.post(
+        'https://royal-backend-1-hzkn.onrender.com',
+        {
+          email,
+          password,
+        }
+      );
 
       onSuccess(res.data.token);
-      toast.success("Welcome Admin");
+      toast.success('Welcome Admin');
     } catch (err) {
-      toast.error("Invalid credentials");
+      toast.error('Invalid credentials');
     }
   };
 
   return (
-    <form className="admin-login" onSubmit={handleLogin}>
+    <form className='admin-login' onSubmit={handleLogin}>
       <h2>Admin Access</h2>
       <input
-        placeholder="Email"
+        placeholder='Email'
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
-        type="password"
-        placeholder="Password"
+        type='password'
+        placeholder='Password'
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
